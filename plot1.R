fcd62879c054112d85491fbb9e9ff69f15fb734e
esilvav <edgar.silvav@gmail.com>
2015-02-09 16:55:21 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,18 +1,39 @@
-# load the individual household electric power consumption data set
-my_file <- file("household_power_consumption.txt")
-my_df   <- read.csv(my_file, sep = ';', na.strings = "?")
+# Exploratory Data Analysis - plot1.R
 
-# identify the date column for filtering purposes
-my_df$Date <- as.Date(my_df$Date, format = "%d/%m/%Y")
+# EDGAR OMAR SILVA V
+#The following descriptions of the 9 variables in the dataset are taken from the UCI web site:
 
-# subset the data
-my_df_2 <- subset(my_df, subset = (Date == "2007-02-01" | Date == "2007-02-02"))
+# Date: Date in format dd/mm/yyyy
+# Time: time in format hh:mm:ss
+# Global_active_power: household global minute-averaged active power (in kilowatt)
+# Global_reactive_power: household global minute-averaged reactive power (in kilowatt)
+# Voltage: minute-averaged voltage (in volt)
+# Global_intensity: household global minute-averaged current intensity (in ampere)
+# Sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered).
+# Sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry room, containing a washing-machine, a tumble-drier, a refrigerator and a light.
+#Sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric water-heater and an air-conditioner.
 
-# plot the requested histogram
-hist(my_df_2$Global_active_power, col = "red", main = paste("Global Active Power"), 
-     xlab = "Global Active Power (kilowatts)")
 
-# Now export the plot from RStudio as a png at 480x480
+exercise="D:/censo 2010/data/household_power_consumption.txt"
 
-# Here, the histogram will illustrate the predominate recordings to be at a low amount of power consumption
-# over the 2 day period
+# Lee los datos desde 2007-02-01 hasta 2007-02-02
+
+skipLines = (66637 - 2 + 1)
+readLines = (69517 - 66638 + 1)
+colclasses = c("character", "character", "numeric")
+colnames = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+
+# Leer en los datos y establece la clase de la columna de datos y tipo de carácter
+
+power_cnsm <- read.table(exercise, header = TRUE, sep=";", na.strings="?", skip=skipLines, nrows=readLines, col.names = colnames, colClasses=colclasses)
+
+# Crea el archivo de formato .PNG
+png("plot1.png", width = 480, height = 480) 
+
+# establecer la familia de fuentes en el dispositivo de png.
+par(mar = c(4, 4, 2, 2), family="serif")
+
+# Dibuja el histograma
+with(power_cnsm, hist(Global_active_power, main = "Global Active Power EOSV", xlab = "Global Active Power (kilowatts)", col = "red", ty=1))
+# Cierre
+dev.off()
